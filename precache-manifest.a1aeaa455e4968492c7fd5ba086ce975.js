35cafc0e082725146d787cf1ea7ff5ec35234a1f
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-08-01 19:07:11 +0300

    Updates

---
+++ precache-manifest.a1aeaa455e4968492c7fd5ba086ce975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8bafe4bbc7c2d7d8cfd",
+    "url": "/Shopping-with-react/static/css/main.07b678be.chunk.css"
+  },
+  {
+    "revision": "e8bafe4bbc7c2d7d8cfd",
+    "url": "/Shopping-with-react/static/js/main.e8bafe4b.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/Shopping-with-react/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "bb7b674109fd2c800ac9",
+    "url": "/Shopping-with-react/static/js/2.bb7b6741.chunk.js"
+  },
+  {
+    "revision": "88b661659ed5fa8a3fab",
+    "url": "/Shopping-with-react/static/js/runtime~main.88b66165.js"
+  },
+  {
+    "revision": "f2efa91c685cbb74269a9376fd76d406",
+    "url": "/Shopping-with-react/index.html"
+  }
+];
