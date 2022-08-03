93405f57a051620f75a8124e39df067c0c20455f
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-08-03 09:30:35 +0300

    Add home page details

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,7 +10,10 @@ function Home() {
               <div class="homeDetails">
                   <h1 class="homeHeader">We care what you wear!</h1>
                   <hr/>
-                  <p></p>
+                  <p>You in a hurry to go to work? Or is it that date you've been anticipating for and it's finally here?!
+                    Here at shopping react we've got it all. From jewllery, to men's wear, kid's wear and even our most favorite, our ladies!
+                    Our prices are very affordable and the quality of our products and services will amaze you. Welcome to our brick and motor layout of businness.
+                  </p>
                   </div>
                   </section>
                   </>
